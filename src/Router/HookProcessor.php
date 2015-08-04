ae04f6af3febe08c6916d63924be41b72ec357a6
Kayla Daniels <kayladnls@gmail.com>
2015-08-04 13:09:35 -0400

    Update Hook Typehints

--- src/Router/HookProcessor.php
+++ src/Router/HookProcessor.php
@@ -1,5 +1,9 @@
 <?php namespace Refinery29\Piston\Router;
 
+use Refinery29\Piston\Hooks\HasHooks;
+use Refinery29\Piston\Http\Request;
+use Refinery29\Piston\Http\Response;
+
 trait HookProcessor
 {
     /**
@@ -8,7 +12,7 @@ trait HookProcessor
      * @param $original_response
      * @return Response
      */
-    protected function processPreHooks($item, $request, $original_response)
+    protected function processPreHooks(HasHooks $item, Request $request, Response $original_response)
     {
         $response = $item->getPreHooks()->process([$request, $original_response]);
         return $response instanceof Response ? $response : $original_response;
@@ -20,7 +24,7 @@ trait HookProcessor
      * @param $original_response
      * @return Response
      */
-    protected function processPostHooks(Hookable $item, $request, $original_response)
+    protected function processPostHooks(HasHooks $item, Request $request, Response $original_response)
     {
         $response = $item->getPostHooks()->process([$request, $original_response]);
         return $response instanceof Response ? $response : $original_response;
