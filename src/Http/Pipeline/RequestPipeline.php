bfa48e3243f1e631d2d16f159fe765444939a74e
Kayla Daniels <kayladnls@gmail.com>
2015-08-06 11:58:14 -0400

    Refactored per code review

--- src/Http/Pipeline/RequestPipeline.php
+++ src/Http/Pipeline/RequestPipeline.php
@@ -10,18 +10,13 @@ use Refinery29\Piston\Pipelines\Stages\RequestedFields;
 class RequestPipeline
 {
     /**
-     * @var PipelineBuilder
-     */
-    protected $builder = null;
-
-    /**
      * @var Pipeline
      */
     protected $pipeline;
 
-    public function __construct()
+    public function __construct(PipelineBuilder $builder = null)
     {
-        $builder = $this->getBuilder();
+        $builder = $builder ?:  new PipelineBuilder();
 
         $this->pipeline = $builder->add(new CursorBasedPagination())
             ->add(new OffsetLimitPagination())
@@ -33,19 +28,5 @@ class RequestPipeline
     public function process($payload)
     {
         return $this->pipeline->process($payload);
-    }
-
-    public function setBuilder(PipelineBuilder $builder)
-    {
-        $this->builder = $builder;
-    }
-
-    public function getBuilder()
-    {
-        if ($this->builder == null) {
-            $this->builder = new PipelineBuilder();
-        }
-
-        return $this->builder;
     }
 }
