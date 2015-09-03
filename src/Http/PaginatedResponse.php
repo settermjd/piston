43066c254207472cfef1bf7ab9ea4a14231153d9
Kayla Daniels <kayladnls@gmail.com>
2015-09-02 23:30:20 -0400

    Add OffsetLimit to Paginated Response

--- src/Http/PaginatedResponse.php
+++ src/Http/PaginatedResponse.php
@@ -5,6 +5,8 @@ namespace Refinery29\Piston\Http;
 trait PaginatedResponse
 {
     protected $pagination;
+    protected $offset;
+    protected $limit;
 
     public function setPaginationCursors(array $cursor)
     {
@@ -14,5 +16,16 @@ trait PaginatedResponse
     public function getPaginationCursors()
     {
         return $this->pagination;
+    }
+
+    public function setOffsetLimit($offset, $limit)
+    {
+        $this->offset = $offset;
+        $this->limit = $limit;
+    }
+
+    public function getOffsetLimit()
+    {
+        return [$this->offset, $this->limit];
     }
 }
