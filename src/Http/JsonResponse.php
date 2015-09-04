b062b1a0a8ddc8621d225462860e61846da1f926
Kayla Daniels <kayladnls@gmail.com>
2015-09-03 23:56:57 -0400

    make cs

--- src/Http/JsonResponse.php
+++ src/Http/JsonResponse.php
@@ -5,4 +5,16 @@ namespace Refinery29\Piston\Http;
 class JsonResponse extends \Symfony\Component\HttpFoundation\JsonResponse
 {
     use PaginatedResponse;
+
+    protected $errors = [];
+
+    public function addError($error)
+    {
+        $this->errors[] = $error;
+    }
+
+    public function getErrors()
+    {
+        return $this->errors;
+    }
 }
