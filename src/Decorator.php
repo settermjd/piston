9383508a5cd083e294cc40d64cca5d8ed899b506
Kayla Daniels <kayladnls@gmail.com>
2015-09-14 13:05:51 -0400

    added new function and doc blocks

--- src/Decorator.php
+++ src/Decorator.php
@@ -4,5 +4,13 @@ namespace Refinery29\Piston;
 
 interface Decorator
 {
+    /**
+     * @param Piston $app
+     */
+    public function __construct(Piston $app);
+
+    /**
+     * @return Piston
+     */
     public function register();
 }
