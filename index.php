aeb03692434c02bec6542f606b885fc1a23ce382
Kayla Daniels <kayladnls@gmail.com>
2015-10-06 10:41:15 -0400

    Added example of Group level Middleware

--- index.php
+++ index.php
@@ -3,10 +3,23 @@
 // INCLUDED ONLY FOR REFERENCE/EXAMPLE WILL BE REMOVED PRIOR TO MERGE!!!!!
 
 use League\Container\Container;
+use Refinery29\ApiOutput\Resource\ResourceFactory;
+use Refinery29\Piston\Response;
 use Refinery29\Piston\Stubs\FooController;
 
 require "vendor/autoload.php";
 
+
+class MiddlewareTwo implements \League\Pipeline\StageInterface
+{
+    public function process($payload)
+    {
+        /** @var Response $response */
+        $response = $payload->getResponse();
+
+        $response->setErrors(ResourceFactory::errorCollection([ResourceFactory::error('something', 'blach')]));
+    }
+}
 
 class Middleware implements \League\Pipeline\StageInterface
 {
@@ -15,7 +28,8 @@ class Middleware implements \League\Pipeline\StageInterface
         $payload->getSubject()->group('/prefix', function ($router) {
 
             $router->get('/something', FooController::class . "::test")->setName('something'); // also still have convenience http methods (get, post, put etc
-        });
+        })
+        ->addMiddleWare(new \MiddlewareTwo);
     }
 }
 
