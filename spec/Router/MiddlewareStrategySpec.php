051552ea764ac2f2cf98a0a5c6c2f8da171f3310
Kayla Daniels <kayladnls@gmail.com>
2015-10-30 22:24:10 -0400

    Tests

--- spec/Router/MiddlewareStrategySpec.php
+++ spec/Router/MiddlewareStrategySpec.php
@@ -4,12 +4,12 @@ namespace spec\Refinery29\Piston\Router;
 
 use League\Container\ContainerInterface;
 use League\Pipeline\Pipeline;
-use League\Route\Route;
 use PhpSpec\ObjectBehavior;
 use Refinery29\Piston\Request;
 use Refinery29\Piston\RequestFactory;
 use Refinery29\Piston\Response;
 use Refinery29\Piston\Router\MiddlewareStrategy;
+use Refinery29\Piston\Router\Route;
 use Refinery29\Piston\Router\RouteGroup;
 use Refinery29\Piston\Stubs\FooController;
 
@@ -30,6 +30,7 @@ class MiddlewareStrategySpec extends ObjectBehavior
     public function it_can_dispatch_controller(Route $route, FooController $foo, Request $request, Response $response)
     {
         $route->getParentGroup()->willReturn(false);
+        $route->getPipeline()->willReturn(new Pipeline());
         $foo->test($request, $response, [])->willReturn($response);
 
         $this->setResponse($response)
@@ -42,9 +43,27 @@ class MiddlewareStrategySpec extends ObjectBehavior
     public function it_handles_group_middleware(RouteGroup $group, Route $route, FooController $foo, Request $request, Response $response)
     {
         $route->getParentGroup()->willReturn($group);
+        $route->getPipeline()->willReturn(new Pipeline());
+
         $group->getPipeline()->willReturn(new Pipeline());
 
         $group->getPipeline()->shouldBeCalled();
+
+        $this->setResponse($response)
+            ->setRequest($request);
+
+        $this->getResponse()->shouldReturn($response);
+
+        $this->dispatch(
+            [$foo, 'test'], [], $route);
+    }
+
+    public function it_handles_route_middleware(Route $route, FooController $foo, Request $request, Response $response)
+    {
+        $route->getParentGroup()->willReturn(false);
+        $route->getPipeline()->willReturn(new Pipeline());
+
+        $route->getPipeline()->shouldBeCalled();
 
         $this->setResponse($response)
             ->setRequest($request);
