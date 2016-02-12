87b57e545c8fca294f5e80933e6b650c0e024830
Andreas Möller <am@localheinz.com>
2016-02-12 21:43:27 +0100

    Fix: Move stubs into appropriate namespace

--- spec/Stubs/FooController.php
+++ spec/Stubs/FooController.php
@@ -6,7 +6,7 @@
  * For the full copyright and license information, please view
  * the LICENSE file that was distributed with this source code.
  */
-namespace Refinery29\Piston\Stubs;
+namespace spec\Refinery29\Piston\Stubs;
 
 use Refinery29\ApiOutput\Resource\ResourceFactory;
 use Refinery29\Piston\ApiResponse;
