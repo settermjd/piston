ef4d415a07d75ce709ff3b37dbc6f98aba0c1710
Andreas Möller <am@localheinz.com>
2016-02-12 21:48:51 +0100

    Fix: Use singular for namespace

--- spec/Stub/TestException.php
+++ spec/Stub/TestException.php
@@ -6,7 +6,7 @@
  * For the full copyright and license information, please view
  * the LICENSE file that was distributed with this source code.
  */
-namespace spec\Refinery29\Piston\Stubs;
+namespace spec\Refinery29\Piston\Stub;
 
 class TestException extends \Exception
 {
